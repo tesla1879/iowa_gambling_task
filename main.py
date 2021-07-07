91f2bd2a5eb57d65464b10ac04cb805d38765c52
Soran Ghadri <soran.gdr.cs@gmail.com>
2021-07-07 05:54:28 +0430

    Create a custom window containing a button

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
 import gi
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gtk
+
+
+class MainWindow(Gtk.Window):
+    def __init__(self):
+        Gtk.Window.__init__(self, title="button click")
+
+        # Button
+        self.button = Gtk.Button(label="click here")
+        self.button.connect("clicked", self.button_clicked)
+        self.add(self.button)
+
+    def button_clicked(self, Widget):
+        print("Game time")
+
+
+
+window = MainWindow()
+window.connect("destroy", Gtk.main_quit)
+window.show_all()
+Gtk.main()
