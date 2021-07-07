a735c12c2c9aea1fe3934524d6bb9623a9b68a22
Soran Ghadri <soran.gdr.cs@gmail.com>
2021-07-07 07:14:14 +0430

    Create listbox Create a form containing a checkbox and a switch

--- main.py
+++ main.py
@@ -6,17 +6,31 @@ from gi.repository import Gtk
 class MainWindow(Gtk.Window):
     def __init__(self):
         Gtk.Window.__init__(self, title="button click")
-        self.grid = Gtk.Grid()
-        self.add(self.grid)
-        # Button
-        self.button = Gtk.Button(label="button")
-        self.button.connect("clicked", self.button_clicked)
-        self.button2 = Gtk.Button(label="add label")
-        self.button2.connect("clicked", self.add_label)
-        # self.add(self.button)
+        self.set_border_width(10)
+        list_box = Gtk.ListBox()
+        list_box.set_selection_mode(Gtk.SelectionMode.NONE)
+        self.add(list_box)
 
-        self.grid.add(self.button)
-        self.grid.attach_next_to(self.button2, self.button, Gtk.PositionType.RIGHT, 1, 7)
+        # Checkbox
+        row1 = Gtk.ListBoxRow()
+        box1 = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=100)
+        row1.add(box1)
+        label = Gtk.Label("This is a test label for checkbox:")
+        check = Gtk.CheckButton()
+        box1.pack_start(label, True, True, 0)
+        box1.pack_start(check, True, True, 0)
+        list_box.add(row1)
+
+        # Toggle
+        row2 = Gtk.ListBoxRow()
+        box2 = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=100)
+        row2.add(box2)
+        label = Gtk.Label("This is a test label for switch: ")
+        switch = Gtk.Switch()
+        box2.pack_start(label, True, True, 0)
+        box2.pack_start(switch, True, True, 0)
+
+        list_box.add(row2)
 
     def button_clicked(self, Widget):
         print("Game time")
