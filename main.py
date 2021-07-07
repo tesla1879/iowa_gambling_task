1bfe02009829f4af25ebd3074edf1017a0770fb0
Soran Ghadri <soran.gdr.cs@gmail.com>
2021-07-07 06:20:16 +0430

    Try Gtk.Grid() Add buttons to the grid

--- main.py
+++ main.py
@@ -6,27 +6,23 @@ from gi.repository import Gtk
 class MainWindow(Gtk.Window):
     def __init__(self):
         Gtk.Window.__init__(self, title="button click")
-        self.box = Gtk.Box(10)
-        self.add(self.box)
+        self.grid = Gtk.Grid()
+        self.add(self.grid)
         # Button
         self.button = Gtk.Button(label="button")
         self.button.connect("clicked", self.button_clicked)
-        self.box.pack_start(self.button, True, True, 0)
-
-
         self.button2 = Gtk.Button(label="add label")
         self.button2.connect("clicked", self.add_label)
-        self.box.pack_start(self.button2, True, True, 0)
         # self.add(self.button)
 
+        self.grid.add(self.button)
+        self.grid.attach_next_to(self.button2, self.button, Gtk.PositionType.RIGHT, 1, 7)
 
     def button_clicked(self, Widget):
         print("Game time")
 
     def add_label(self, Widget):
-        self.label = Gtk.Label()
-        self.label.set_label("this is a test label")
-        self.add(self.label)
+        print("button 2")
 
 
 
