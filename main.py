279c9b2f5f34e9b94deeae8e1979fedd8bb058cb
Soran Ghadri <soran.gdr.cs@gmail.com>
2021-07-07 06:08:50 +0430

    Add Gtk box to the window Add 2 buttons to the box

--- main.py
+++ main.py
@@ -6,14 +6,27 @@ from gi.repository import Gtk
 class MainWindow(Gtk.Window):
     def __init__(self):
         Gtk.Window.__init__(self, title="button click")
-
+        self.box = Gtk.Box(10)
+        self.add(self.box)
         # Button
-        self.button = Gtk.Button(label="click here")
+        self.button = Gtk.Button(label="button")
         self.button.connect("clicked", self.button_clicked)
-        self.add(self.button)
+        self.box.pack_start(self.button, True, True, 0)
+
+
+        self.button2 = Gtk.Button(label="add label")
+        self.button2.connect("clicked", self.add_label)
+        self.box.pack_start(self.button2, True, True, 0)
+        # self.add(self.button)
+
 
     def button_clicked(self, Widget):
         print("Game time")
+
+    def add_label(self, Widget):
+        self.label = Gtk.Label()
+        self.label.set_label("this is a test label")
+        self.add(self.label)
 
 
 
